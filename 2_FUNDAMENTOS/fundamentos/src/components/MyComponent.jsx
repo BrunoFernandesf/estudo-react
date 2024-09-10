1ea8217bcd93ba255a754b10c41637ae448a87f1
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-09-10 10:30:50 -0300

    hierarquia de components

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+
+    return(
+        <div>
+            <h2>Estou sendo utilizado em diversos locais</h2>
+        </div>
+    )
+}
+export default MyComponent
--- 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.jsx
@@ -1,3 +1,5 @@
+import MyComponent from "./MyComponent"
+
 const TemplateExpressions = () => {
 
     const name = "Bruno"
@@ -10,6 +12,7 @@ const TemplateExpressions = () => {
         <div>
             <h1>Olá {name}, tudo bem?</h1>
             <p>Você tem {data.age} e trabalha como {data.job}</p>
+            <MyComponent/>
         </div>
     )
 }
