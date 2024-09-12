c4d47cae0d98d1dfe7a71c5d754c373a088ede4b
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-09-12 15:40:23 -0300

    imagens publicas

---
+++ 3_AVANCANDO/avancando-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Seção 03</h1>
+        <img src='/img1.jpg' alt='Paisagem'/>
+      </div>
+    </>
+  )
+}
+
+export default App
