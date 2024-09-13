4aaf175c58fe4e8882b1c5f15a6c3a82c2f5987b
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-09-13 14:28:37 -0300

    imagens em src

--- 3_AVANCANDO/avancando-react/src/App.jsx
+++ 3_AVANCANDO/avancando-react/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Img from './assets/img1.jpg'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +13,9 @@ function App() {
       <div>
         <h1>Seção 03</h1>
         <img src='/img1.jpg' alt='Paisagem'/>
+        <div>
+          <img src={Img} alt='Paisagem'/>
+        </div>
       </div>
     </>
   )
